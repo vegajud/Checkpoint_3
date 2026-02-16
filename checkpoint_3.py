8c7591c68efad34f0579707ab0750d05ea9b188d
vegajud <judit.vega.info@gmail.com>
2026-02-16 19:24:29 +0000

    checkpoint_3

---
+++ checkpoint_3.py
@@ -0,0 +1,20 @@
+text = "Hey there, this is a string." #1
+number = 25
+hobbies = ["music", "books", "learn", "videogames"]
+temperature = 5
+is_cold = temperature < 15
+if is_cold:
+  print("Don't forget your scarf!")
+else: 
+  print("Enjoy the heat!")
+print (is_cold)
+
+three_letters = text[0:3] #2
+first_element = hobbies[0] #3
+new_number = number + 10 #4
+last_element = hobbies[-1] #5
+names = "harry,alex,susie,jared,gail,conner"
+names_list = names.split(",") #6
+upper_text = text[0:3].upper() + text [3:] #7
+print(f"I think you should know that my favorite number is {number}") #8
+print("hello world") #9
