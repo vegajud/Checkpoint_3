bc2502d9a2d71ee12a1f8dbfbecac48ed71dcc71
vegajud <judit.vega.info@gmail.com>
2026-02-16 20:29:44 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-name = 'Kristine'
-age = 12
-product = 'Python eLearning Course'
-from_account = 'Jordan'
-
-greeting = f"Product Purchase: {product} - Hi {name}, you are listed as {age} years old. - {from_account}"
-
-print(greeting)
