5e55a9b51610b1795ad86938f8aa00fb415eccd1
vegajud <judit.vega.info@gmail.com>
2026-02-16 20:29:11 +0100

    Delete index_based_string.py

--- index_based_string.py
+++
@@ -1,7 +0,0 @@
-name = 'Kristine'
-age = 12
-product = 'Python eLearning Course'
-
-greeting = "Hi {0}, you are listed as {1} years old. You have purchased: {2}...".format(name, age, product)
-
-print(greeting)
