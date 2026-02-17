4c3f05eb2fb006eed2ba888cf2bd576a9b97baba
vegajud <judit.vega.info@gmail.com>
2026-02-17 11:56:11 +0100

    Update checkpoint_3.py

--- checkpoint_3.py
+++ checkpoint_3.py
@@ -6,25 +6,36 @@ boolean = True
 
 #Ejercicio 2
 three_letters = text[0:3]
+print(three_letters)
 
 #Ejercicio 3
 first_element = hobbies[0]
+print(first_element)
 
 #Ejercicio 4
 new_number = number + 10
+print(new_number)
 
 #Ejercicio 5
 last_element = hobbies[-1]
+print(last_element)
 
 #Ejercicio 6
 names = "harry,alex,susie,jared,gail,conner"
 names_list = names.split(",")
+print(names_list)
 
 #Ejercicio 7
 upper_text = text[0:3].upper() + text [3:]
+print(upper_text)
 
 #Ejercicio 8
 print(f"I think you should know that my favorite number is {number}")
 
 #Ejercicio 9
 print("hello world")
+
+#Ejercicio adicional
+another_text = "Hola"
+another_text = another_text.replace("Hola", "adiós")
+print(another_text)
